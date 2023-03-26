fa521ec5b785c39c8a75ca05168703fae8ebabbd
lzp725 <128158392+lzp725@users.noreply.github.com>
2023-03-26 20:23:46 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -228,7 +228,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            发送
+            Go
           </button>
           <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
